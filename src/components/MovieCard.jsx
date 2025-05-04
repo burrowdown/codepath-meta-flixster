64217b8e541270cb52d9b44171b80302a2c13ed6
Danny Burrow <burrowingdan@gmail.com>
2025-05-04 15:41:34 -0700

    milestone 1, build the basics

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,7 +1,16 @@
 import { useState } from "react"
 
+// TODO: call the config endpoint to get the base URL
+const BASE_URL = "https://image.tmdb.org/t/p/w185"
+
 const MovieCard = ({ movie }) => {
-  return <>{movie.title}</>
+  return (
+    <div className="movie-card">
+      <img src={`${BASE_URL}${movie.poster_path}`} alt={movie.title} />
+      <div className="movie-title">{movie.title}</div>
+      <div className="movie-rating">Rating: {movie.vote_average}</div>
+    </div>
+  )
 }
 
 export default MovieCard
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react"
 import MovieCard from "./MovieCard"
+import "./MovieList.css"
 
 const MovieList = () => {
   const [movies, setMovies] = useState([])
