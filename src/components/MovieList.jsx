0ee5776bd6d308eed034c2e9d391fc47ccf64c48
Danny Burrow <burrowingdan@gmail.com>
2025-05-04 16:54:57 -0700

    build out the load more button

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react"
+import { useEffect, useRef, useState } from "react"
 import MovieCard from "./MovieCard"
 import "./MovieList.css"
 
@@ -6,38 +6,63 @@ const MovieList = () => {
   const [movies, setMovies] = useState([])
   const [loading, setLoading] = useState(true)
   const [error, setError] = useState(null)
+  const [page, setPage] = useState(1)
+  const isMounted = useRef(false)
+  const scrollPosition = useRef(0)
+
+  const BASE_URL =
+    "https://api.themoviedb.org/3/movie/now_playing?language=en-US"
+  const options = {
+    method: "GET",
+    headers: {
+      accept: "application/json",
+      Authorization: `Bearer ${import.meta.env.VITE_API_READ_ACCESS_TOKEN}`,
+    },
+  }
+
+  const fetchMovies = async (isLoadMore) => {
+    try {
+      const response = await fetch(`${BASE_URL}&page=${page}`, options)
+      if (!response.ok) {
+        throw new Error("Network response was not ok")
+      }
+      const data = await response.json()
+      setMovies((prevMovies) => [...prevMovies, ...data.results])
+    } catch (error) {
+      setError(error)
+    } finally {
+      setLoading(false)
+    }
+  }
+
+  const loadMore = () => {
+    scrollPosition.current = window.scrollY
+    setPage((prevPage) => prevPage + 1)
+    setLoading(true)
+  }
 
   useEffect(() => {
-    const url =
-      "https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1"
-    const options = {
-      method: "GET",
-      headers: {
-        accept: "application/json",
-        Authorization: `Bearer ${import.meta.env.VITE_API_KEY}`,
-      },
+    if (isMounted.current) {
+      fetchMovies()
+    } else {
+      isMounted.current = true
     }
+  }, [page])
 
-    const fetchMovies = async () => {
-      try {
-        const response = await fetch(url, options)
-        if (!response.ok) {
-          throw new Error("Network response was not ok")
-        }
-        const data = await response.json()
-        setMovies(data.results)
-        console.log(data.results)
-      } catch (error) {
-        setError(error)
-      } finally {
-        setLoading(false)
-      }
+  useEffect(() => {
+    // Restore the scroll position after movies are loaded
+    if (!loading) {
+      window.scrollTo(0, scrollPosition.current)
     }
-    fetchMovies()
-  }, [])
+  }, [loading])
+
   if (loading) {
-    // TODO: something fancy here
-    return <div>Loading...</div>
+    return (
+      <div className="loading">
+        Loading...
+        <img className="loading-spinner" src="/movie.png" alt="Loading" />
+      </div>
+    )
   }
   if (error) {
     return <div>Error: {error.message}</div>
@@ -46,11 +71,16 @@ const MovieList = () => {
     return <div>No movies found</div>
   }
   return (
-    <div className="movie-list">
-      {movies.map((movie) => (
-        <MovieCard key={movie.id} movie={movie} />
-      ))}
-    </div>
+    <>
+      <div className="movie-list">
+        {movies.map((movie) => (
+          <MovieCard key={movie.id} movie={movie} />
+        ))}
+      </div>
+      <button onClick={loadMore} className="load-more-button">
+        Load More
+      </button>
+    </>
   )
 }
 
