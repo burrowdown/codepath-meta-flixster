1092b75c304867a71e9c444980246f219ce46e7a
Danny Burrow <burrowingdan@gmail.com>
2025-05-09 16:34:01 -0700

    create StatusActions component

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,10 +1,4 @@
-import { useEffect, useState } from "react"
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import { faHeart as heartSolid } from "@fortawesome/free-solid-svg-icons"
-import { faHeart as heartRegular } from "@fortawesome/free-regular-svg-icons"
-import { faEye as eyeSolid } from "@fortawesome/free-solid-svg-icons"
-import { faEye as eyeRegular } from "@fortawesome/free-regular-svg-icons"
-import { setMovieState } from "../utils/utils"
+import StatusActions from "./StatusActions"
 
 const FALLBACK_URL = "https://image.tmdb.org/t/p"
 
@@ -15,62 +9,28 @@ const MovieCard = ({
   alreadyFavorited,
   alreadyWatched,
 }) => {
-  const [isFavorite, setIsFavorite] = useState(alreadyFavorited)
-  const [isWatched, setIsWatched] = useState(alreadyWatched)
   if (!movie) return null
 
   const base_url =
     config && config.images ? config.images.base_url : FALLBACK_URL
   const url = `${base_url}/w185${movie.poster_path}`
 
-  const addFavorite = async (e) => {
-    e.stopPropagation()
-    const body = JSON.stringify({
-      media_type: "movie",
-      media_id: movie.id,
-      favorite: !isFavorite,
-    })
-    const url = `https://api.themoviedb.org/3/account/${
-      import.meta.env.VITE_ACCOUNT_ID
-    }/favorite`
-
-    setMovieState(url, body, setIsFavorite)
-  }
-
-  const addWatched = async (e) => {
-    e.stopPropagation()
-    const body = JSON.stringify({
-      media_type: "movie",
-      media_id: movie.id,
-      watchlist: !isWatched,
-    })
-    const url = `https://api.themoviedb.org/3/account/${
-      import.meta.env.VITE_ACCOUNT_ID
-    }/watchlist`
-
-    setMovieState(url, body, setIsWatched)
-  }
-
-  useEffect(() => {
-    setIsFavorite(alreadyFavorited)
-  }, [alreadyFavorited])
-
-  useEffect(() => {
-    setIsWatched(alreadyWatched)
-  }, [alreadyWatched])
-
   return (
-    <div className="movie-card" onClick={() => setCurrentMovie(movie)}>
+    <div
+      className="movie-card"
+      onClick={() =>
+        setCurrentMovie({
+          id: movie.id,
+          isFavorite: alreadyFavorited,
+          isWatched: alreadyWatched,
+        })
+      }
+    >
       <div className="movie-card-icons">
-        <FontAwesomeIcon
-          icon={isWatched ? eyeSolid : eyeRegular}
-          color={isWatched ? "#1D84B5" : "#888"}
-          onClick={(e) => addWatched(e)}
-        />
-        <FontAwesomeIcon
-          icon={isFavorite ? heartSolid : heartRegular}
-          color={isFavorite ? "#FF4242" : "#888"}
-          onClick={(e) => addFavorite(e)}
+        <StatusActions
+          movie={movie}
+          alreadyFavorited={alreadyFavorited}
+          alreadyWatched={alreadyWatched}
         />
       </div>
       <img src={url} alt={`poster for ${movie.title}`} />
---
+++ src/components/StatusActions.jsx
@@ -0,0 +1,66 @@
+import { useEffect, useState } from "react"
+import { setMovieState } from "../utils/utils"
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faHeart as heartSolid } from "@fortawesome/free-solid-svg-icons"
+import { faHeart as heartRegular } from "@fortawesome/free-regular-svg-icons"
+import { faEye as eyeSolid } from "@fortawesome/free-solid-svg-icons"
+import { faEye as eyeRegular } from "@fortawesome/free-regular-svg-icons"
+
+const StatusActions = ({ alreadyFavorited, alreadyWatched, movie }) => {
+  const [isFavorite, setIsFavorite] = useState(alreadyFavorited)
+  const [isWatched, setIsWatched] = useState(alreadyWatched)
+
+  const addFavorite = async (e) => {
+    e.stopPropagation()
+    const body = JSON.stringify({
+      media_type: "movie",
+      media_id: movie.id,
+      favorite: !isFavorite,
+    })
+    const url = `https://api.themoviedb.org/3/account/${
+      import.meta.env.VITE_ACCOUNT_ID
+    }/favorite`
+
+    setMovieState(url, body, setIsFavorite)
+  }
+
+  const addWatched = async (e) => {
+    e.stopPropagation()
+    const body = JSON.stringify({
+      media_type: "movie",
+      media_id: movie.id,
+      watchlist: !isWatched,
+    })
+    const url = `https://api.themoviedb.org/3/account/${
+      import.meta.env.VITE_ACCOUNT_ID
+    }/watchlist`
+
+    setMovieState(url, body, setIsWatched)
+  }
+
+  useEffect(() => {
+    setIsFavorite(alreadyFavorited)
+  }, [alreadyFavorited])
+
+  useEffect(() => {
+    setIsWatched(alreadyWatched)
+  }, [alreadyWatched])
+
+  return (
+    <>
+      <FontAwesomeIcon
+        icon={isWatched ? eyeSolid : eyeRegular}
+        color={isWatched ? "#1D84B5" : "#888"}
+        onClick={(e) => addWatched(e)}
+      />
+      <FontAwesomeIcon
+        icon={isFavorite ? heartSolid : heartRegular}
+        color={isFavorite ? "#FF4242" : "#888"}
+        onClick={(e) => addFavorite(e)}
+      />
+    </>
+  )
+}
+
+export default StatusActions
